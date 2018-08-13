2040034a0afcbbeb8bab2cd6cbace0371b94e7e3
Nikki A <nikki.apostolakis@gmail.com>
2018-08-13 15:22:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,17 +2,17 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
- 
+
     num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
- 
+
     if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
+      resp.write "You Win" #instead of puts
     else
-      resp.write "You Lose"
+      resp.write "You Lose" #instead of puts
     end
- 
+
     resp.finish
   end
 
